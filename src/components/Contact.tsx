9ff224559897fce63838242a3b0328e9dbf45d4b
codewithabhay10 <abhaymadan22@gmail.com>
2026-02-04 22:36:33 +0530

    add social media link to Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -128,6 +128,23 @@ const Contact = () => {
               </motion.a>
 
               <motion.a
+                href="https://x.com/abhaymadan26"
+                target="_blank"
+                rel="noopener noreferrer"
+                whileHover={{ scale: 1.1 }}
+                className="w-10 h-10 rounded-full flex items-center justify-center"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="white"
+                  viewBox="0 0 24 24"
+                  className="w-5 h-5"
+                >
+                  <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z" />
+                </svg>
+              </motion.a>
+
+              <motion.a
                 href="https://www.linkedin.com/in/abhay-madan-470804290/"
                 target="_blank"
                 rel="noopener noreferrer"
